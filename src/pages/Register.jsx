09fcaba4d70d1749c1f30d83d9eb8f7b639f0659
rrizvi3 <128552797+rrizvi3@users.noreply.github.com>
2023-09-21 16:49:14 -0500

    added login, registration pages
    .

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,58 @@
-export default function Register() {
-  return <div>Register</div>;
-}
+import React, { useState } from 'react';
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Here, you can add code to send the registration data to the server for user creation.
+    // You may also want to add validation and error handling.
+
+    // For demonstration purposes, we'll log the registration data for now.
+    console.log('Username:', username);
+    console.log('Password:', password);
+
+    // After successful registration, you can redirect the user to the login page or another page.
+  };
+
+  return (
+    <div className="registration-container">
+      <h2>Registration</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
