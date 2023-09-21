55c68576af321fc4f3780842b1f6b3da76281ddc
rrizvi3 <128552797+rrizvi3@users.noreply.github.com>
2023-09-21 01:50:04 -0500

    login, client

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+//import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Login';
+import ClientRegistration from './ClientRegistration';
+//import Profile from './Profile';
+//import FuelQuoteForm from './FuelQuoteForm';
+//import FuelQuoteHistory from './FuelQuoteHistory';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/ClientRegistration" component={ClientRegistration} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
